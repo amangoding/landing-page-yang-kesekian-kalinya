a27249324c7c8b7cc768daa89ec1343d73fc185f
amangoding <kesumaaa34@gmail.com>
2026-05-14 22:24:02 +0800

    fix dark mode

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-function Darkmode() {
-    let element = document.body;
-    let toggleDarkmode = document.querySelector("#toggleDarkmode")
-    let body = document.getElementsByTagName("body")
+// function Darkmode() {
+//     let element = document.body;
+//     let toggleDarkmode = document.querySelector("#toggleDarkmode")
+//     let body = document.getElementsByTagName("body")
 
-    if (toggleDarkmode.checked) {
-       element.classList.toggle("dark-mode")
-    } else {
-       body
-    }
-}
+//     if (toggleDarkmode.checked) {
+//        element.classList.toggle("dark-mode")
+//     } else {
+//        body
+//     }
+// }
 
 function darkMode() {
-    const element = document.body;
+    let element = document.body;
+
     element.classList.toggle("dark-mode")
+}
+
+function toggledark() {
+    const root = document.body;
+    const isDark = root.dataset.theme === 'dark';
+
+    if (root.dataset.theme === isDark) {
+        'dark'
+    } else {
+        'light'
+    }
+
+    root.dataset.theme = isDark ? 'light' : 'dark'; 
 }
